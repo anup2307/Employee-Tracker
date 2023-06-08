38252e6b2444a83b3857fc603ea88cc022db3dab
ap_boot <sahapri@gmail.com>
2023-06-08 13:12:10 -0400

    Added update role part and correctes few other issues

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const db =  mysql.createConnection({
     database: 'employee_db'
 })
 
-function prompt(){
+function questions(){
     inquirer.prompt([
     {
         type:'list',
@@ -22,122 +22,116 @@ function prompt(){
             'Add Department',
             'Add Roles',
             'Add Employee',
-            'Update Employee Role'
+            'Update Employee Role',
+            'Exit'
         ]
     }
 ])
 .then((answer) => {
     console.log(answer);
-    switch (answer.action){
-        case 'View All Employees':
-            selectEmployee();
-            break;
-        case 'Add Employee':
-            inquirer.prompt([
-                {
-                    type: 'input',
-                    name: 'first_name',
-                    message: 'Enter Employee first name: '
-                },
-                {
-                    type: 'input',
-                    name: 'last_name',
-                    message: 'Enter Employee last name: '
-                },
-                {
-                    type: 'list',
-                    name: 'role',
-                    message: 'Enter Employee role : ',
-                    choices:[
-                        'Sales Lead',
-                        'Salesperson',
-                        'Lead Engineer',
-                        'Software Engineer',
-                        'Account Manager',
-                        'Accountant',
-                        'Legal Team Lead',
-                        'Lawyer',
-                        'HR Staff'
-                    ]
-                },
-                {
-                    type: 'list',
-                    name: 'manager',
-                    message: 'Enter Employee manager name: ',
-                    choices : [
-                        'None',
-                        'Georgi Facello',
-                        'Saniya Kalloufi',
-                        'Divier Reistad',
-                        'Arif Merlo',
-                        'Bader Swan'
-                    ]
-                },
-            ])
-            .then(({first_name,last_name,role,manager}) => {
-                addEmployee(first_name,last_name,role,manager);
-            });
+    switch (answer.action){        
+        case 'View All Departments':
+            selectDepartment();
             break;
         case 'View All Roles':
             selectRole();
             break;
-        case 'View All Departments':
-            selectDepartment();
-            break;
-        case 'Add Roles':
-            addRole();
+        case 'View All Employees':
+            selectEmployee();
             break;
         case 'Add Department':
             addDepartment();
             break;
+        case 'Add Roles':
+            addRole();
+            break;
+        case 'Add Employee':
+            addEmployee();
+            break;
         case 'Update Employee Role':
             updateRole();
             break;
-        default:
-            console.log('err');
+        case 'Exit':
+            db.end();
             break;
     }         
     })
 };
 
-function getRoleid(role,callback){
-    let roleId =0;
-    console.log('role:',role);
+var roleArr = [];
+function getRoles() {
+  db.query("SELECT * FROM role", function(err, res) {
+    if (err) throw err
+    for (var i = 0; i < res.length; i++) {
+      roleArr.push({value:res[i].id, name: res[i].title});
+    }
+  })
+  return roleArr;
+}
 
-    db.query('select id from role where title = ?',role, function(err,results) {
-        if (err){
-            console.log('error in roleid');
-        }else{
-            roleId = results[0].id;
-            console.log('roleid:',roleId);  
-            return callback(roleId)  
+var managerlist=[{name: 'None',value:''}];
+function getManagerlist(){
+    db.query('select id,concat(first_name," ",last_name) as managerName from employee where manager_id is NULL', function(err,res){
+        if(err) throw err
+        for (var i=0; i<res.length; i++){
+            managerlist.push({name:res[i].managerName,value:res[i].id})
         }
-        console.log('roleid1:',roleId);
-    })
+    }) 
+    return managerlist;
+}
+
+var employeeArr= [];
+function getEmployeelist(){
+    db.query('select id,concat(first_name," ",last_name) as employeeName from employee', function(err,res){
+        if(err) throw err
+        for (var i=0; i<res.length; i++){
+            employeeArr.push({name:res[i].employeeName,value:res[i].id})
+            console.log(employeeArr);
+        }
+    }) 
+    
+    return employeeArr;
+}
+
+var departmentArr= [];
+function getDepartmentlist(){
+    db.query('select * from department', function(err,res){
+        if(err) throw err
+        for (var i=0; i<res.length; i++){
+            departmentArr.push({name:res[i].dep_name,value:res[i].id})
+        }
+    }) 
+    return departmentArr;
+}
+
+function selectDepartment(){
+    db.query('select id as DEPT_Id, dep_name as DEPT_Name from department', function (err, results) {
+        if (err){console.log('Error viewing table')} 
+        else{ console.table(results);
+              questions();
+        }
+    });
+}
+
+function selectRole(){
+    db.query('select role.id as Role_id, title as Roles, salary, dep_name as Department_name from role join department on role.department_id=department.id', function (err, results) {
+        if (err){console.log('Error viewing table')} 
+        else{ console.table(results);
+              questions();
+        }
+    });
 }
 
 function selectEmployee(){
     console.log('in employee')
     db.query('select employee.id as Emp_Id, first_name as First_Name, last_name as Last_name, title as Job_Title, salary, dep_name as Department_name, m.emp_name as manager_name from employee, role, department, (select id,  concat(first_name," ", last_name) as emp_name from employee) M where employee.role_id=role.id and 	role.department_id=department.id    and	m.id=employee.manager_id   order by emp_id; ',
      function (err, results) {
-        err ? console.log('error in viewing Employee table') :console.table(results);
-        prompt();
+        if (err){console.log('Error viewing table')} 
+        else{ console.table(results);
+              questions();
+        }
     });
 };
-
-function selectDepartment(){
-    db.query('select id as DEPT_Id, dep_name as DEPT_Name from department', function (err, results) {
-        err ? console.log('error in viewing Department table') : console.table(results);
-        prompt();
-    });
-}
-
-function selectRole(){
-    db.query('select role.id as Role_id, title as Roles, salary, dep_name as Department_name from role join department on role.department_id=department.id', function (err, results) {
-        err ? console.log('error in viewing Roles table') : console.table(results);
-        prompt();
-    });
-}
 
 function addDepartment(){
     inquirer.prompt([
@@ -147,10 +141,13 @@ function addDepartment(){
             message: 'Enter Department name: '
         }])
         .then(({dep_name}) => {
-            db.query('insert into department (dep_name) values (?)',dep_name,function (err, results) {
-                err ? console.log('error in viewing Department table') : prompt();})
-        })
-        
+            db.query('insert into department (dep_name) values (?)', dep_name, function (err, results) {
+                if (err) {console.log('Error in adding Department')}
+                else{
+                   console.log('Department details added Successfully'); 
+                   questions();
+                }})
+        })        
 }
 
 function addRole(){
@@ -167,43 +164,89 @@ function addRole(){
         },
         {
             type:'list',
-            name: 'dep_name',
+            name: 'dep_id',
             message:'choose the Department:',
-            choices:[
-                'Marketing',
-                'Finance',
-                'Human Resources',
-                'Production',
-                'Development',
-                'Quality Management',
-                'Legal',
-                'Customer Service'
-            ]
+            choices: getDepartmentlist()
         }
     ])
-    .then(({role_name,salary,dep_name})=>{
-        console.log(role_name,salary,dep_name);
+    .then(({role_name,salary,dep_id})=>{
+        db.query('insert into role (title, salary, department_id) values(?,?,?)',[role_name, salary, dep_id], function(err,res){
+            if (err) {console.log('Error in adding Role')}
+                else{
+                   console.log('Added Successfully'); 
+                   questions();
+                }
+        })             
     })
 }
 
-function addEmployee(first_name,last_name,role,manager){
-    const managerId = manager.split(' ');
-    let managerid = 0;
-    db.query('select id from employee where first_name = ? and  last_name=?',managerId, function (err, results) {
-        if(err){ console.log('error in managerid');}
-        else
-        { 
-            managerid = results[0].id;
-            return managerid;
+function addEmployee(){
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'first_name',
+            message: 'Enter Employee first name: '
+        },
+        {
+            type: 'input',
+            name: 'last_name',
+            message: 'Enter Employee last name: '
+        },
+        {
+            type: 'list',
+            name: 'role',
+            message: 'Enter Employee role : ',
+            choices: getRoles()
+        },
+        {
+            type: 'list',
+            name: 'manager',
+            message: 'Enter Employee manager name: ',
+            choices : getManagerlist()
+        },
+    ])
+    .then(({first_name,last_name,role,manager}) => {
+        if(manager.length=== 0){ 
+            db.query('insert into employee (first_name, last_name, role_id,manager_id) values (?,?,?,null)',[first_name,last_name,role],function (err, results) {
+                if (err) {console.log('Error in adding employee')}
+                else{
+                   console.log('Employee details added Successfully'); 
+                   questions();}})
+        }else{
+            db.query('insert into employee (first_name, last_name, role_id, manager_id) values (?,?,?,?)',[first_name,last_name,role,manager],function (err, results) {
+             if (err) {console.log('Error in adding employee')}
+             else{
+                console.log('Employee details added Successfully'); 
+                questions();}
+            });  
+        }
+    });   
+}
+
+function updateRole(){
+    inquirer.prompt([
+        {
+            type: 'list',
+            name:'employee_id',
+            message: 'Which employee role do you want to update? ',
+            choices: getEmployeelist()
+        },
+        {
+            type:'list',
+            name:'role',
+            message: 'Which role do you want to assign to the selected employee? ',
+            choices: getRoles()
+        }
+])
+.then(({employee_id,role}) => {
+    db.query('update employee set role_id =? where id = ?',[role,employee_id], function(err,res){
+        if (err){ console.log('Error in Updating employee role')}
+        else{ 
+            console.log('Updated Employee role');
+            questions();
         }
     })
-    
-    let roleId = getRoleid(role,function(response){
-                    return response;
-                 });
-    console.log('hiiii::',roleId);
-    db.query('insert into employee (first_name, last_name, role_id, manager_id) values (?,?,?,?)',[first_name,last_name,roleId,managerid],function (err, results) {
-        if(err){ console.log('error in adding employee',err);}});
-    prompt();
+})
 }
-prompt();
+
+questions();
